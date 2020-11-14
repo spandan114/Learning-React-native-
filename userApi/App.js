9a5c6c9adca6758093893a505080de3c5cd75203
spandan joshi <spandanj685@gmail.com>
2020-11-14 17:19:51 +0530

    random user api

--- userApi/App.js
+++ userApi/App.js
@@ -1,7 +1,8 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Axios from 'axios'
 import {Button} from 'native-base'
+import Users from './components/Users'
 
 const key = 1
 const URL = 'https://randomuser.me/api/'
@@ -12,20 +13,49 @@ export default function App() {
 
   const fetchDetails = async () =>{
     try {
-       const {data} = await axios.get(URL)
-
-       console.log(data.results[0])
-
-    } catch (error) {
+       const {data} = await Axios.get(URL)
+       setDetails(data.results[0])
+      } catch (error) {
       console.log(error)
     }
   }
 
-  return (
-    <View>
-      <Text>Hii</Text>
+  useEffect(()=>{
+    fetchDetails()
+  },[])
+
+  if(!details){
+    return(
+    <View style={styles.container}>
+      <Text style={{color:"#fff",alignItems:"center",justifyContent:"center"}}>Loding....</Text>
     </View>
-  )
+    )
+  }else{
+        return (
+        <View style={styles.container}>
+          <View>
+            <Users details={details} />
+
+            <Button rounded 
+              onPress={() => fetchDetails()}
+              style={styles.button}>
+            <Text>New user</Text>
+          </Button>
+          </View>
+        </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"#222831"
+  },
+  button:{
+    marginTop:30,
+    paddingHorizontal:30,
+  }
+})
